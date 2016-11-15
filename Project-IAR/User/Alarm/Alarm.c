914629d136081c5b0dea3e3d4d8b90921b301373
ZJYC <zhaojingyangchun@163.com>
2016-11-15 18:56:45 +0800

    Try to modify HMI driver

--- Project-IAR/User/Alarm/Alarm.c
+++ Project-IAR/User/Alarm/Alarm.c
@@ -23,27 +23,27 @@ static Caution caution =
 */
 static result Alarm_open(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitTypeDef GPIO_InitStruct;
     //初始化4个LED
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3,GPIO_PIN_RESET);
-	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3,GPIO_PIN_RESET);
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
     //初始化蜂鸣器
     __HAL_RCC_GPIOE_CLK_ENABLE();
     HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1,GPIO_PIN_RESET);
-	GPIO_InitStruct.Pin = GPIO_PIN_1;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-	
-	AddPrivateBuf(&Alarm,(uint8_t *)&caution,sizeof(Caution));
-	//设置状态
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+    
+    AddPrivateBuf(&Alarm,(uint8_t *)&caution,sizeof(Caution));
+    //设置状态
     SET_STATE(Alarm.state,STATE_OPEN);
     RESET_STATE(Alarm.state,STATE_CLOSE);
-	
+    
     return true;
 }
 /*
@@ -60,9 +60,9 @@ static result Alarm_open(void)
 */
 static result Alarm_close(void)
 {
-	HAL_GPIO_DeInit(GPIOC,GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+    HAL_GPIO_DeInit(GPIOC,GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
     HAL_GPIO_DeInit(GPIOE,GPIO_PIN_1);
-	//设置状态
+    //设置状态
     RESET_STATE(Alarm.state,STATE_OPEN);
     SET_STATE(Alarm.state,STATE_CLOSE);
 
@@ -82,7 +82,7 @@ static result Alarm_close(void)
 */
 static result Alarm_detect(void)
 {
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
 
     return true;
 }
@@ -100,7 +100,7 @@ static result Alarm_detect(void)
 */
 static result Alarm_command(uint8_t * command, uint32_t param)
 {
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
     return true;
 }
 /*
@@ -117,39 +117,39 @@ static result Alarm_command(uint8_t * command, uint32_t param)
 */
 static result Alarm_set(uint32_t LEDx, uint32_t State)
 {
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
-	
-	switch(LEDx)
-	{
-		
-		case 0:{
-					if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_0,GPIO_PIN_SET);
-					if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_0,GPIO_PIN_RESET);
-					break;
-				}
-		case 1:{
-					if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_1,GPIO_PIN_SET);
-					if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_1,GPIO_PIN_RESET);
-					break;
-				}
-		case 2:{
-					if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_SET);
-					if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_RESET);
-					break;
-				}
-		case 3:{
-					if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_SET);
-					if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_RESET);
-					break;
-				}
-		case 4:{
-					if(State == 0)HAL_GPIO_WritePin(GPIOE,GPIO_PIN_1,GPIO_PIN_SET);
-					if(State == 1)HAL_GPIO_WritePin(GPIOE,GPIO_PIN_1,GPIO_PIN_RESET);
-					break;
-				}
-		default:break;
-	}
-	
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    
+    switch(LEDx)
+    {
+        
+        case 0:{
+                    if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_0,GPIO_PIN_SET);
+                    if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_0,GPIO_PIN_RESET);
+                    break;
+                }
+        case 1:{
+                    if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_1,GPIO_PIN_SET);
+                    if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_1,GPIO_PIN_RESET);
+                    break;
+                }
+        case 2:{
+                    if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_SET);
+                    if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_RESET);
+                    break;
+                }
+        case 3:{
+                    if(State == 0)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_SET);
+                    if(State == 1)HAL_GPIO_WritePin(GPIOC,GPIO_PIN_3,GPIO_PIN_RESET);
+                    break;
+                }
+        case 4:{
+                    if(State == 0)HAL_GPIO_WritePin(GPIOE,GPIO_PIN_1,GPIO_PIN_SET);
+                    if(State == 1)HAL_GPIO_WritePin(GPIOE,GPIO_PIN_1,GPIO_PIN_RESET);
+                    break;
+                }
+        default:break;
+    }
+    
     return true;
 }
 /*
@@ -166,10 +166,10 @@ static result Alarm_set(uint32_t LEDx, uint32_t State)
 */
 static result Alarm_puts(uint32_t LEDx, uint8_t * Directive, uint32_t Cnt)
 {
-	p_Caution caution = (p_Caution)Alarm.data;
-	
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
-	
+    p_Caution caution = (p_Caution)Alarm.data;
+    
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    
     osMutexWait(AlarmMutex,osWaitForever);
     caution->Counter_1[LEDx] = Cnt,
     caution->Directive[LEDx] = Directive;
@@ -190,7 +190,7 @@ static result Alarm_puts(uint32_t LEDx, uint8_t * Directive, uint32_t Cnt)
 */
 static result Alarm_gets(uint32_t SendAddr, uint8_t * start, uint32_t length)
 {
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
 
     return true;
 }
@@ -210,13 +210,13 @@ static result Alarm_timing_process(uint32_t Param1, uint32_t Param2, uint32_t Pa
 {
     uint16_t Cnt1;
     uint8_t * Temp = 0;
-	p_Caution caution = (p_Caution)Alarm.data;
-	
-	if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
+    p_Caution caution = (p_Caution)Alarm.data;
+    
+    if(CHECK_STATE(Alarm.state,STATE_CLOSE) && Alarm.d_open() != true)return false;
 
-	Param1 = Param1;
-	Param2 = Param2;
-	Param3 = Param3;
+    Param1 = Param1;
+    Param2 = Param2;
+    Param3 = Param3;
     //由于这是在定时器回调里面，所以不能使之阻塞
     //osMutexWait(AlarmMutex,osWaitForever);
     //循环计数
@@ -246,31 +246,31 @@ static result Alarm_timing_process(uint32_t Param1, uint32_t Param2, uint32_t Pa
         }
     }
     //osMutexRelease(AlarmMutex);
-	taskEXIT_CRITICAL();
-	return true;	
+    taskEXIT_CRITICAL();
+    return true;    
 }
 
 deviceModule Alarm = 
 {
-	#if(CUR_BOARD == MAIN_BOARD)
-	.name               = "Alarm_0",	
-	#elif(CUR_BOARD == BOARD_1)
-	.name               = "Alarm_1",	
-	#elif(CUR_BOARD == BOARD_2)
-	.name               = "Alarm_2",
-	#elif(CUR_BOARD == BOARD_3)
-	.name               = "Alarm_3",	
+    #if(CUR_BOARD == MAIN_BOARD)
+    .name               = "Alarm_0",    
+    #elif(CUR_BOARD == BOARD_1)
+    .name               = "Alarm_1",    
+    #elif(CUR_BOARD == BOARD_2)
+    .name               = "Alarm_2",
+    #elif(CUR_BOARD == BOARD_3)
+    .name               = "Alarm_3",    
     #endif
-	.state              = STATE_CLOSE,
-	.next               = 0x00,
-	.DA                 = Public,
-	.d_open             = Alarm_open,
-	.d_close            = Alarm_close,
-	.d_detect           = Alarm_detect,
-	.d_command          = Alarm_command,
-	.d_set              = Alarm_set,
-	.d_puts             = Alarm_puts,
-	.d_gets             = Alarm_gets,
+    .state              = STATE_CLOSE,
+    .next               = 0x00,
+    .DA                 = Public,
+    .d_open             = Alarm_open,
+    .d_close            = Alarm_close,
+    .d_detect           = Alarm_detect,
+    .d_command          = Alarm_command,
+    .d_set              = Alarm_set,
+    .d_puts             = Alarm_puts,
+    .d_gets             = Alarm_gets,
     .d_timing_proceee   = Alarm_timing_process
 };
 
--- Project-IAR/User/HMI/HMI.c
+++ Project-IAR/User/HMI/HMI.c
@@ -34,51 +34,51 @@ uint8_t CurPageID = 0xFF,CurItem = 0;
 PageInfTypedef PageAll[] = 
 {
     {
-	"大北屋",
-	2,
+    "大北屋",
+    2,
     {
     {0,"c0",14,1},
-	{0,"c1",15,0},
-	{0,"c2",16,1},
+    {0,"c1",15,0},
+    {0,"c2",16,1},
     {0,"n0",18,12},
-	{0,"n1",19,13},
-	{0,"n2",20,14},
+    {0,"n1",19,13},
+    {0,"n2",20,14},
     }
     },
     {
-	"南屋",
-	3,
+    "南屋",
+    3,
     {
     {0,"c0",15,1},
-	{0,"c1",16,0},
-	{0,"c2",17,0},
+    {0,"c1",16,0},
+    {0,"c2",17,0},
     {0,"n0",18,23},
-	{0,"n1",19,34},
-	{0,"n2",20,56},
+    {0,"n1",19,34},
+    {0,"n2",20,56},
     }
-	},
+    },
     {
-	"大门",
-	4,
+    "大门",
+    4,
     {
     {0,"c0",15,0},
-	{0,"c1",16,1},
-	{0,"c2",17,1},
+    {0,"c1",16,1},
+    {0,"c2",17,1},
     {0,"n0",18,1},
-	{0,"n1",19,2},
-	{0,"n2",20,3},
+    {0,"n1",19,2},
+    {0,"n2",20,3},
     }
-	},
+    },
     {
-	"庭院",
-	5,
+    "庭院",
+    5,
     {
     {0,"c0",15,1},
-	{0,"c1",16,1},
-	{0,"c2",17,0},
+    {0,"c1",16,1},
+    {0,"c2",17,0},
     {0,"n0",18,7},
-	{0,"n1",19,8},
-	{0,"n2",20,9},
+    {0,"n1",19,8},
+    {0,"n2",20,9},
     }
     }
 };
@@ -93,6 +93,7 @@ PageInfTypedef PageAll[] =
 
 result HMI_ExecInstruction(uint8_t * Data,uint8_t Length);
 static result HMI_SetTime(p_DS1307TimeTypedef Time);
+static result HMI_SendInstruction(uint8_t * Command,uint8_t Type);
 #if 0//发送指令
 
 static uint8_t * SendInstruct_page(uint8_t * ID)
@@ -278,9 +279,10 @@ static uint8_t * SendInstruct_rest(void)
 
 #endif
 
-static result HMI_SendInstruction(uint8_t * Command)
+static result HMI_SendInstruction(uint8_t * Command,uint8_t Type)
 {
     uint8_t Buf[30] = {0x00},Len = 0,cnt = 0;
+    p_Uart2PrivateTypedef U2PD = (p_Uart2PrivateTypedef)usart_1.data;
     
     strcpy(Buf,Command);
     
@@ -289,26 +291,31 @@ static result HMI_SendInstruction(uint8_t * Command)
     Buf[Len + 0] = 0xff;
     Buf[Len + 1] = 0xff;
     Buf[Len + 2] = 0xff;
+    //开始接收数据
+    U2PD->Recvd = 0x00;
     
     usart_1.d_puts(0,Buf,Len + 3);
     
-    while(1)
+    while(Type == 0xff)
     {
         uint8_t i = 0;
-        p_Uart2PrivateTypedef U2PD = (p_Uart2PrivateTypedef)usart_1.data;
         
-        osDelay(5 + cnt * 5);
+        osDelay(100);
         
-        if(U2PD->Recvd == 0xff)//表示收到数据
+        if(U2PD->Recvd == 0xff)//如果收到数据，判断是否为成功
         {
             //要掠过前导0 
             while(U2PD->Uart2Buf[i] == 0)i++;
             if(i >= 100)return false;
-            U2PD->Recvd = 0x00;
-            if(HMI_ExecInstruction(&U2PD->Uart2Buf[i],100 - i) == HMI_ReturnTrue)break;
+            if(HMI_ExecInstruction(&U2PD->Uart2Buf[i],100 - i) == HMI_ReturnTrue)
+            {
+                U2PD->Recvd = 0x00;
+                return true;
+            }
             
         }
-        if(cnt ++ > 20)return false;
+        //0.1S内没有收到，重试10次，最后返回false
+        if(cnt ++ > 10)return false;
         usart_1.d_puts(0,Buf,Len + 3);
     }
     
@@ -323,32 +330,32 @@ static result HMI_SendInstruction(uint8_t * Command)
 
 static result SaveHMIValue(uint8_t PageID,uint8_t ItemID,uint8_t NewValue)
 {
-	uint8_t i = 0,j = 0;
-	
-	for(i = 0;i < sizeof(PageAll)/sizeof(PageAll[0]);i ++)
-	{
-		if(PageAll[i].ID == PageID)
-		{
-			for(j = 0;j < 6; j ++)
-			{
-				if(PageAll[i].Item[j].ID == ItemID)
-				{
-					//PageAll[i].Item[j].Changed = 0xff;
-					if(PageAll[i].Item[j].Value == 0x00)PageAll[i].Item[j].Value = 1;
+    uint8_t i = 0,j = 0;
+    
+    for(i = 0;i < sizeof(PageAll)/sizeof(PageAll[0]);i ++)
+    {
+        if(PageAll[i].ID == PageID)
+        {
+            for(j = 0;j < 6; j ++)
+            {
+                if(PageAll[i].Item[j].ID == ItemID)
+                {
+                    //PageAll[i].Item[j].Changed = 0xff;
+                    if(PageAll[i].Item[j].Value == 0x00)PageAll[i].Item[j].Value = 1;
                     else PageAll[i].Item[j].Value = 0;
-				}
-			}
-			
-		}
-	}
-	
-	return true;
-	
+                }
+            }
+            
+        }
+    }
+    
+    return true;
+    
 }
 
 static result SyncPage(uint8_t PageID)
 {
-	uint8_t i = 0,j = 0,k = 0,Buf[30] = {0x00},Buf_1[10] = {0x00};
+    uint8_t i = 0,j = 0,k = 0,Buf[30] = {0x00},Buf_1[10] = {0x00};
     
     if(PageID == 0)
     {
@@ -356,98 +363,95 @@ static result SyncPage(uint8_t PageID)
         HMI_SetTime(DS1307Time);
     }
     
-	for(i = 0;i < sizeof(PageAll)/sizeof(PageAll[0]);i ++)
-	{
-		if(PageAll[i].ID == PageID)
-		{
-			for(j = 0;j < 6;j ++)
-			{
+    for(i = 0;i < sizeof(PageAll)/sizeof(PageAll[0]);i ++)
+    {
+        if(PageAll[i].ID == PageID)
+        {
+            for(j = 0;j < 6;j ++)
+            {
                 //for(k = 0;k < 3;k ++)
                 {
                     strcpy(Buf,PageAll[i].Item[j].Name);
                     strcat(Buf,".val=");
                     sprintf(Buf_1,"%d",PageAll[i].Item[j].Value);
                     strcat(Buf,Buf_1);
-                    HMI_SendInstruction(Buf);
+                    HMI_SendInstruction(Buf,0xff);
                     //osDelay(20);
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 result HMI_ExecInstruction(uint8_t * Data,uint8_t Length)
 {
-	
-	if(Data == 0)return false;
-	
-	switch(*Data)
-	{
-		case HMI_RETURN_00:{return HMI_ReturnErr;}
-		case HMI_RETURN_01:{return HMI_ReturnTrue;}
-		case HMI_RETURN_02:{return HMI_ReturnErr;}
-		case HMI_RETURN_03:{return HMI_ReturnErr;}
-		case HMI_RETURN_04:{return HMI_ReturnErr;}
-		case HMI_RETURN_05:{return HMI_ReturnErr;}
-		case HMI_RETURN_11:{return HMI_ReturnErr;}
-		case HMI_RETURN_12:{return HMI_ReturnErr;}
-		case HMI_RETURN_1A:{return HMI_ReturnErr;}
-		case HMI_RETURN_1B:{return HMI_ReturnErr;}
-		case HMI_RETURN_1C:{return HMI_ReturnErr;}
-		case HMI_RETURN_1D:{return HMI_ReturnErr;}
-		case HMI_RETURN_1E:{return HMI_ReturnErr;}
-		case HMI_RETURN_1F:{return HMI_ReturnErr;}
-		case HMI_RETURN_20:{return HMI_ReturnErr;}
-		case HMI_RETURN_23:{return HMI_ReturnErr;}
-		case HMI_RETURN_HotArea			:{
-											HMI_ProcotolHotArea_Typedef *HotArea = (HMI_ProcotolHotArea_Typedef*)Data;
-											SaveHMIValue(HotArea->CurPageID,HotArea->ButtonID,HotArea->TouchEvent);
-											SyncPage(HotArea->CurPageID);
+    
+    if(Data == 0)return false;
+    
+    switch(*Data)
+    {
+        case HMI_RETURN_00:{return HMI_ReturnErr;}
+        case HMI_RETURN_01:{return HMI_ReturnTrue;}
+        case HMI_RETURN_02:{return HMI_ReturnErr;}
+        case HMI_RETURN_03:{return HMI_ReturnErr;}
+        case HMI_RETURN_04:{return HMI_ReturnErr;}
+        case HMI_RETURN_05:{return HMI_ReturnErr;}
+        case HMI_RETURN_11:{return HMI_ReturnErr;}
+        case HMI_RETURN_12:{return HMI_ReturnErr;}
+        case HMI_RETURN_1A:{return HMI_ReturnErr;}
+        case HMI_RETURN_1B:{return HMI_ReturnErr;}
+        case HMI_RETURN_1C:{return HMI_ReturnErr;}
+        case HMI_RETURN_1D:{return HMI_ReturnErr;}
+        case HMI_RETURN_1E:{return HMI_ReturnErr;}
+        case HMI_RETURN_1F:{return HMI_ReturnErr;}
+        case HMI_RETURN_20:{return HMI_ReturnErr;}
+        case HMI_RETURN_23:{return HMI_ReturnErr;}
+        case HMI_RETURN_HotArea         :{
+                                            HMI_ProcotolHotArea_Typedef *HotArea = (HMI_ProcotolHotArea_Typedef*)Data;
+                                            SaveHMIValue(HotArea->CurPageID,HotArea->ButtonID,HotArea->TouchEvent);
+                                            SyncPage(HotArea->CurPageID);
                                             break;
-										}
-		case HMI_RETURN_CurPageID		:{
-											HMI_ReturnCurPageID_Typedef * PageID = (HMI_ReturnCurPageID_Typedef *)Data;
-											SyncPage(PageID->CurPageID);
-											CurPageID = PageID->CurPageID;
-											break;
-										}
-		case HMI_RETURN_TouchPos			:{break;}
-		case HMI_RETURN_SleepTouch		:{HMI_SendInstruction("sendme");break;}
-		case HMI_RETURN_ReturnStr		:{break;}
-		case HMI_RETURN_ReturnVal		:{break;}
-		case HMI_RETURN_AutoInterSleep	:{break;}
-		case HMI_RETURN_AutoAware		:{break;}
-		case HMI_RETURN_StartUpSuccess	:{break;}
-		case HMI_RETURN_SdUpdate			:{break;}
-		case HMI_RETURN_DataTransparentCpt:{break;}
-		case HMI_RETURN_DataTransparentRdy:{break;}
-		default:break;
-	}
+                                        }
+        case HMI_RETURN_CurPageID       :{
+                                            HMI_ReturnCurPageID_Typedef * PageID = (HMI_ReturnCurPageID_Typedef *)Data;
+                                            SyncPage(PageID->CurPageID);
+                                            CurPageID = PageID->CurPageID;
+                                            break;
+                                        }
+        case HMI_RETURN_TouchPos            :{break;}
+        case HMI_RETURN_SleepTouch      :{HMI_SendInstruction("sendme",0x00);break;}
+        case HMI_RETURN_ReturnStr       :{break;}
+        case HMI_RETURN_ReturnVal       :{break;}
+        case HMI_RETURN_AutoInterSleep  :{break;}
+        case HMI_RETURN_AutoAware       :{break;}
+        case HMI_RETURN_StartUpSuccess  :{break;}
+        case HMI_RETURN_SdUpdate            :{break;}
+        case HMI_RETURN_DataTransparentCpt:{break;}
+        case HMI_RETURN_DataTransparentRdy:{break;}
+        default:break;
+    }
 }
 
 
 result HMI_Task(uint32_t Type,uint32_t cnt)
 {
-	uint8_t i = 0;
-	p_Uart2PrivateTypedef U2PD = (p_Uart2PrivateTypedef)usart_1.data;
+    uint8_t i = 0;
+    p_Uart2PrivateTypedef U2PD = (p_Uart2PrivateTypedef)usart_1.data;
     if(CHECK_STATE(usart_1.state,STATE_CLOSE))usart_1.d_open();
-	if(U2PD->Recvd == 0xff)//表示收到数据
-	{
-        
+    if(U2PD->Recvd == 0xff)//表示收到数据
+    {
         //要掠过前导0 
         while(U2PD->Uart2Buf[i] == 0)i++;
         if(i >= 100)return false;
-		HMI_ExecInstruction(&U2PD->Uart2Buf[i],100 - i);
-        for(i = 0;i < 100;i ++)
-        {
-            U2PD->Uart2Buf[i] = 0;
-        }
-        U2PD->Recvd = 0x00;    
-        Alarm.d_puts(LED2,"11110000",1);
-	}
-    if(cnt % 10 == 0)
+        HMI_ExecInstruction(&U2PD->Uart2Buf[i],100 - i);
+        for(i = 0;i < 100;i ++)U2PD->Uart2Buf[i] = 0;
+        Alarm.d_puts(LED2,"1100",1);
+        //恢复数据的接收
+        U2PD->Recvd = 0x00;
+    }
+    if(cnt % 30 == 0)
     {
-        HMI_SendInstruction("sendme");
+        HMI_SendInstruction("sendme",0x00);
     }
     if(cnt % 50 == 0)
     {
@@ -458,24 +462,23 @@ result HMI_Task(uint32_t Type,uint32_t cnt)
 
 static result HMI_SetTime(p_DS1307TimeTypedef Time)
 {
-	//t1.txt="2016-08-20"
-	//t0.txt=23:35
-	uint8_t Buf[30] = {0x00};
-	
-	if(CurPageID == 0)
-	{
-	
-		sprintf(Buf,"t1.txt=\"20%02d-%02d-%02d\"",Time->year,Time->mon,Time->day);
-		
-		HMI_SendInstruction(Buf);
-		
-		sprintf(Buf,"t2.txt=\"%02d:%02d\"",Time->hour,Time->min);
-		
-		HMI_SendInstruction(Buf);
-	
-	}
+    //t1.txt="2016-08-20"
+    //t0.txt=23:35
+    uint8_t Buf[30] = {0x00};
+    
+    if(CurPageID == 0)
+    {
+    
+        sprintf(Buf,"t1.txt=\"20%02d-%02d-%02d\"",Time->year,Time->mon,Time->day);
+        
+        HMI_SendInstruction(Buf,0xff);
+        
+        sprintf(Buf,"t2.txt=\"%02d:%02d\"",Time->hour,Time->min);
+        
+        HMI_SendInstruction(Buf,0xff);
+    
+    }
 }
-
 
 
 
